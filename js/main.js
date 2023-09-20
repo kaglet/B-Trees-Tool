c74d834656cb004452ad4089249a87aef6336249
Aidan <2427688@students.wits.ac.za>
2023-09-20 19:07:53 +0200

    Fixed error after file seperation

--- js/main.js
+++ js/main.js
@@ -10,111 +10,140 @@ var frameNumber = 0;
 var canvas;
 var rootNode;
 var graphics;
-const tree = new BTree(2); // 2 is the max degree
-var insertButton = document.getElementById('insertBtn');
-var removeButton = document.getElementById('removeBtn');
+var tree; // 2 is the max degree
+// var insertButton = document.getElementById('insertBtn');
+// var removeButton = document.getElementById('removeBtn');
 
+var createTreeStarted = true;
 function init() {
     try {
 		canvas = document.getElementById("canvas");
 		graphics = canvas.getContext("2d");
+        document.getElementById("max-degree").focus();
+        document.getElementById("insert-delete-section").style.display = "none";
 	} catch(e) {
 		document.getElementById("canvasholder").innerHTML = "An error occurred while initializing graphics.";
-	}
-
-	// A B-Tree with minimum degree 3
-    tree.insert(100);
-    tree.traverse();
-    tree.insert(200);
-    tree.traverse();
-    tree.insert(300);
-    tree.traverse();
-
-    tree.insert(150);
-    tree.traverse();
-    tree.insert(250);
-    tree.traverse();
-    tree.insert(350);
-    tree.traverse();
-
-    tree.insert(50);
-    tree.traverse();
-    tree.insert(275);
-    tree.traverse();
-    tree.insert(380);
-    tree.traverse();
-
-    // console.log("Traversal of the constructed tree is:");
-    // tree.traverse();
-
-    console.log("The Tree:");
-    graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-    drawTree(tree.root, canvas.width / 2 - 60, 50);
-
-    
+	}    
 }
 
-// user input and insert that value into the tree
 var insertValue;
 function insertKey() {
-    try {
-		insertValue = document.getElementById("insert").value;
-        //ensure a traverse is called after an insert to allow for cleaning tree
-        try{
-            if (insertValue === ""){
-                console.log("Enter a number");
-            } else {
-                tree.insert(parseInt(insertValue));
-                tree.traverse();
+    if (!createTreeStarted) {
+        document.getElementById("error-message").innerHTML  = "";
+        try {
+            insertValue = document.getElementById("insert").value;
+            //ensure a traverse is called after an insert to allow for cleaning tree
+            try{
+                if (insertValue === ""){
+                    console.log("Enter a number");
+                    document.getElementById("error-message").innerHTML  = "Please enter a key to insert";
+                } else {
+                    tree.insert(parseInt(insertValue));
+                    tree.traverse();
+                    document.getElementById("insert").focus();
+                    document.getElementById("error-message").innerHTML  = "";
+                }
+            } catch(e) {
+                console.log(e);
             }
+            graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+            console.log("The Tree:");
+            drawTree(tree.root, canvas.width / 2 - 60, 50);
+            document.getElementById("insert").value = null;
         } catch(e) {
             console.log(e);
         }
-        graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-        console.log("The Tree:");
-        drawTree(tree.root, canvas.width / 2 - 60, 50);
-        document.getElementById("insert").value = null;
-	} catch(e) {
-        console.log(e);
-	}
+    } else {
+        document.getElementById("error-message").innerHTML  = "Please enter a max degree and select 'Custom Tree'";
+    }
 }
+
 // user input and remove that value from the tree
 var removeValue;
 function removeKey() {
-    try {
-		removeValue = document.getElementById("delete").value;
-        //ensure a traverse is called after a removal to allow for cleaning tree
-        try{
-            if (removeValue === ""){
-                console.log("Enter a number");
-            } else {
-                tree.remove(parseInt(removeValue));
-                tree.traverse();
+    if (!createTreeStarted) {
+        document.getElementById("error-message").innerHTML  = "";
+        try {
+            removeValue = document.getElementById("delete").value;
+            //ensure a traverse is called after a removal to allow for cleaning tree
+            try{
+                if (removeValue === ""){
+                    console.log("Enter a number");
+                    document.getElementById("error-message").innerHTML  = "Please enter a key to remove";
+                } else {
+                    tree.remove(parseInt(removeValue));
+                    tree.traverse();
+                    document.getElementById("delete").focus();
+                    document.getElementById("error-message").innerHTML  = "";
+
+                }
+            } catch(e) {
+                console.log(e);
             }
+            graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+            console.log("The Tree:");
+            drawTree(tree.root, canvas.width / 2 - 60, 50);
+            document.getElementById("delete").value = null;
         } catch(e) {
             console.log(e);
         }
-        graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-        console.log("The Tree:");
-        drawTree(tree.root, canvas.width / 2 - 60, 50);
-        document.getElementById("delete").value = null;
-	} catch(e) {
-        console.log(e);
-	}
+    } else {
+        document.getElementById("error-message").innerHTML  = "Please enter a max degree and select 'Custom Tree'";
+    }
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Your initialization code here
-//     init(); // Call your init function
-// });
+var maxDegree;
+function createTree() {
+    if (createTreeStarted) {
+        try {
+            maxDegree = document.getElementById("max-degree").value;
+            //ensure a traverse is called after a removal to allow for cleaning tree
+            try{
+                if (maxDegree === ""){
+                    console.log("Enter a max degree value");
+                    document.getElementById("error-message").innerHTML  = "Enter a max degree value before creating a tree";
+                } else {
+                    document.getElementById("error-message").innerHTML  = "";
+                    tree = new BTree(parseInt(maxDegree));
+                    document.getElementById("custom-tree-btn").innerHTML  = "Cancel";
+		            document.getElementById("insert").focus();
+                    createTreeStarted= false;
+                    console.log(document.getElementById("insert-delete-section").style.display)
+                    if (document.getElementById("insert-delete-section").style.display === "none" || document.getElementById("insert-delete-section").style.display === "") {
+                        document.getElementById("insert-delete-section").style.display = "inline"; // Show the section
+                    } else {
+                        document.getElementById("insert-delete-section").style.display = "none"; // Hide the section
+                    }
+                }
+            } catch(e) {
+                console.log(e);
+            }
+            document.getElementById("max-degree").value = null;
+        } catch(e) {
+            console.log(e);
+        }
+        
+    } else {
+        console.log("Tree Creation Canceled");
+        graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+        document.getElementById("custom-tree-btn").innerHTML  = "Custom tree";
+        document.getElementById("max-degree").focus();
+        createTreeStarted= true;
+        console.log(document.getElementById("insert-delete-section").style.display)
+        if (document.getElementById("insert-delete-section").style.display === "none" || document.getElementById("insert-delete-section").style.display === "") {
+            document.getElementById("insert-delete-section").style.display = "inline"; // Show the section
+        } else {
+            document.getElementById("insert-delete-section").style.display = "none"; // Hide the section
+        }
+    }
+}
 
-//document.addEventListener("DOMContentLoaded", init);
 
-insertButton.addEventListener( 'click', () => {
-    insertKey();    
-} );
+// insertButton.addEventListener( 'click', () => {
+//     insertKey();    
+// } );
 
-removeButton.addEventListener( 'click', () => {
-    removeKey();    
-} );
+// removeButton.addEventListener( 'click', () => {
+//     removeKey();    
+// } );
 
