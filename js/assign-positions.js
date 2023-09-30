dc3fa272594a672faa89c27b20e07fc34ac650a4
Kago <kmotlhabane769@gmail.com>
2023-09-30 17:08:51 +0200

    Try modify original object in imported function

--- js/assign-positions.js
+++ js/assign-positions.js
@@ -1,6 +1,8 @@
 // call this function once logical representation of tree is done.
-export function assignNodePositions(rootNode){
-    console.log(rootNode);
+export function assignNodePositions(obj){
+    console.log("hi");
+    obj.age = 9;
+    console.log('Object age inside function: ' + obj.age);
     // let levels = [];
     // do modification experiment on rootNode when this function is to be called
     // do modification experiments on rootNodes children
