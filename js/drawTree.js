7d741969b6bbea8f360f40b9d5b1ad28d9e5ad5e
luca-vm <127728915+luca-vm@users.noreply.github.com>
2023-10-05 22:50:26 +0200

    fixed wonky arrow heads

--- js/drawTree.js
+++ js/drawTree.js
@@ -1,12 +1,12 @@
 let graphics;
 
 export function drawTree(node, canvas) {
-    graphics = canvas.getContext("2d"); 
+    graphics = canvas.getContext("2d");
     const canvasWidth = canvas.width;
     if (!node) return;
 
     const keys = node.keys;
-   // const keys = node.keys.filter((key) => key.value !== undefined);
+    // const keys = node.keys.filter((key) => key.value !== undefined);
     const nodeWidth = keys.length * 60;
     const nodeSpacing = 40;
     const arrowSize = 15; // Size of the arrowhead
@@ -16,9 +16,9 @@ export function drawTree(node, canvas) {
 
     if (!node.leaf) {
         const numChildren = node.C.length;
-      
+
         const childXPositions = [];
-    
+
         node.C.forEach((child, index) => {
             if (child != undefined) {
                 if (child.keys[index] != undefined) {
@@ -31,17 +31,15 @@ export function drawTree(node, canvas) {
                     }
                     const childX = child.keys[0].x - 30;
                     const childY = child.keys[0].y - 30;
-    
-                    const angle = Math.atan2(childY - (keys[0].y + 30), childX - keys[0].x);
-    
+
                     // Determine if the child is less than or greater than the key
                     const isLessThanKey = childKeys[0].value < keys[index].value;
-    
+
                     // Calculate the arrow starting point
-                    const arrowStartX = isLessThanKey 
-                        ? keys[0].x - nodeWidth / 2 + (index + 1) * 60 + (node.t - 2)*60
-                        : keys[0].x + nodeWidth / 2 - (keys.length - index - 1) * 60 + (node.t - 2)*60;
-    
+                    const arrowStartX = isLessThanKey
+                        ? keys[0].x - nodeWidth / 2 + (index + 1) * 60 + (node.t - 2) * 60
+                        : keys[0].x + nodeWidth / 2 - (keys.length - index - 1) * 60 + (node.t - 2) * 60;
+
                     graphics.save();
                     graphics.beginPath();
                     graphics.moveTo(arrowStartX, keys[0].y + 30); // Arrow starts from the appropriate side of the key
@@ -50,40 +48,47 @@ export function drawTree(node, canvas) {
                     graphics.strokeStyle = "orange";
                     graphics.stroke();
                     graphics.closePath();
-    
+
                     graphics.beginPath();
                     graphics.moveTo(childX + childWidth, childY);
-                    graphics.lineTo(
-                        (childX + childWidth) - arrowSize * Math.cos(angle - Math.PI / 6),
-                        childY - arrowSize * Math.sin(angle - Math.PI / 6)
-                    );
-                    graphics.lineTo(
-                        (childX + childWidth) - arrowSize * Math.cos(angle + Math.PI / 6),
-                        childY - arrowSize * Math.sin(angle + Math.PI / 6)
-                    );
+
+                    // Calculate the angle between the arrow line and the horizontal axis
+                    const angle = Math.atan2(childY - (keys[0].y + 30), childX + childWidth - arrowStartX);
+
+                    // Calculate the coordinates of the arrowhead
+                    const arrowheadX1 = (childX + childWidth) - arrowSize * Math.cos(angle - Math.PI / 6);
+                    const arrowheadY1 = childY - arrowSize * Math.sin(angle - Math.PI / 6);
+                    const arrowheadX2 = (childX + childWidth) - arrowSize * Math.cos(angle + Math.PI / 6);
+                    const arrowheadY2 = childY - arrowSize * Math.sin(angle + Math.PI / 6);
+
+                    // Draw the arrowhead triangle
+                    graphics.lineTo(arrowheadX1, arrowheadY1);
+                    graphics.lineTo(arrowheadX2, arrowheadY2);
                     graphics.fillStyle = "orange";
                     graphics.fill();
                     graphics.closePath();
-    
+
                     graphics.restore();
-    
+
+
+
                     childXPositions.push(childX);
-    
+
                     drawTree(child, canvas);
                 }
             }
         });
     }
-    
+
 }
 
 
 function drawKey(x, y, key, graphics = graphics) {
     const keySize = 30; //size of blue square -- hopefull make into draggable
     graphics.fillStyle = "lightblue";
-    
+
     graphics.fillRect(x, y, keySize, keySize);  //fills blue small rect
-    
+
     graphics.fillStyle = "black";
     graphics.font = "14px Arial";
     graphics.textAlign = "center";
@@ -93,7 +98,7 @@ function drawKey(x, y, key, graphics = graphics) {
 
 export function drawNode(keys, graphics) {
     const nodeHeight = 60;
-   // const validKeys = keys;
+    // const validKeys = keys;
     const validKeys = keys.filter((key) => key.x !== undefined);  //tking away undefined from array
     const nodeWidth = validKeys.length * 60;    //the whole node width (black outlined rects)
 
@@ -105,10 +110,9 @@ export function drawNode(keys, graphics) {
     graphics.font = "14px Arial";
     graphics.textAlign = "center";
     graphics.textBaseline = "middle";
-    
+
     validKeys.forEach((key, index) => {
-        drawKey(key.x - 15, key.y -15, key.value, graphics);
+        drawKey(key.x - 15, key.y - 15, key.value, graphics);
     });
 }
-
 
