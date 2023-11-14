23a9298f5fd6571d9fc53db559816ed3cef37afa
Michael-Sent <msent888@gmail.com>
2023-11-14 11:06:19 +0200

    added home button to user guide

--- js/user-guide.js
+++ js/user-guide.js
@@ -2,16 +2,20 @@ const navLinks = document.querySelectorAll('nav ul li a');
 const contentSections = document.querySelectorAll('section');
 
 function handleNavClick(event) {
-    event.preventDefault();
+    
     const sectionId = event.target.getAttribute('data-section-id');
     
-    contentSections.forEach(section => {
-        if (section.id === sectionId) {
-            section.classList.add('active');
-        } else {
-            section.classList.remove('active');
-        }
-    });
+    if(sectionId!='home-section'){
+        event.preventDefault();
+        contentSections.forEach(section => {
+            if (section.id === sectionId) {
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
+        });
+    }
+    
 }
 
 navLinks.forEach(link => {
