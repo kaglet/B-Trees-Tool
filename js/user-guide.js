4a1c18fe64cb489f36e43f1e345450fc2e811026
Michael-Sent <msent888@gmail.com>
2023-11-15 11:29:41 +0200

    added insertion guide and updated user guide style

--- js/user-guide.js
+++ js/user-guide.js
@@ -2,20 +2,17 @@ const navLinks = document.querySelectorAll('nav ul li a');
 const contentSections = document.querySelectorAll('section');
 
 function handleNavClick(event) {
-    
     const sectionId = event.target.getAttribute('data-section-id');
     
-    if(sectionId!='home-section'){
-        event.preventDefault();
-        contentSections.forEach(section => {
-            if (section.id === sectionId) {
-                section.classList.add('active');
-            } else {
-                section.classList.remove('active');
-            }
-        });
-    }
+    event.preventDefault();
     
+    contentSections.forEach(section => {
+        if (section.id === sectionId) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    });
 }
 
 navLinks.forEach(link => {
